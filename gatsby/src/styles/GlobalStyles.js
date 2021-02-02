6d7a4fa9a2b9305773b926fc9f37c79bfb19fe49
balthazarely <balthazar2120@gmail.com>
2021-02-02 11:44:54 -0700

    removed scroll css

--- gatsby/src/styles/GlobalStyles.js
+++ gatsby/src/styles/GlobalStyles.js
@@ -114,7 +114,22 @@ const GlobalStyles = createGlobalStyle`
   height: 100%;
 }
 
- 
+  /* Scrollbar Styles */
+  body::-webkit-scrollbar {
+    width: 12px;
+  }
+  html {
+    scrollbar-width: thin;
+    scrollbar-color: white rgb(86, 179, 129);
+  }
+  body::-webkit-scrollbar-track {
+    background: white;
+  }
+  body::-webkit-scrollbar-thumb {
+    background-color: rgb(86, 179, 129) ;
+    border-radius: 6px;
+    /* border: 3px solid red; */
+  }
 
   .gatsby-image-wrapper img[src*=base64\\,] {
     image-rendering: -moz-crisp-edges;
