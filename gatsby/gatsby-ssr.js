0a33feb80e8abbe242e07754d56c07c12e8dab72
Balthazar <balthazar.elyj@gmail.com>
2021-09-23 07:34:40 -0600

    new changes

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -5,6 +5,7 @@ import { GlobalProvider } from './src/context/GlobalContext';
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
 );
+
 export function wrapRootElement({ element }) {
   return <GlobalProvider>{element}</GlobalProvider>;
 }
