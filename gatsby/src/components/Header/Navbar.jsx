d323857a7a846ba700d7a5e5458a0572fdc16367
Balthazar <balthazar.elyj@gmail.com>
2023-01-09 19:55:08 -0800

    stufffffff

--- gatsby/src/components/Header/Navbar.jsx
+++ gatsby/src/components/Header/Navbar.jsx
@@ -1,15 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'gatsby';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import { useScrollPosition } from '@n8tb1t/use-scroll-position';
-import { AnchorLink } from 'gatsby-plugin-anchor-links';
-import { device } from '../../utils/breakpoints';
-import NavIcon from './NavIcon';
-import GlobalContext from '../../context/GlobalContext';
-import Menu from './Menu';
-import ToggleSwitch from './ToggleSwitch';
-import downloadFile from '../../assets/resume/Balthazar_Ely_Resume2022.pdf';
+import React, { useState, useContext } from "react";
+import { Link } from "gatsby";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useScrollPosition } from "@n8tb1t/use-scroll-position";
+import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { device } from "../../utils/breakpoints";
+import NavIcon from "./NavIcon";
+import GlobalContext from "../../context/GlobalContext";
+import Menu from "./Menu";
+import ToggleSwitch from "./ToggleSwitch";
+import downloadFile from "../../assets/resume/Balthazar_Ely_Resume_2023.pdf";
 
 const Header = styled.nav`
   padding: 0;
@@ -124,17 +124,13 @@ export default function Navbar() {
 
   return (
     <>
-      <Header
-        className={`sticky-header nav-header ${
-          showScrolling ? 'scrolling' : ''
-        } ${showReveal ? 'reveal-header' : ''}`}
-      >
+      <Header className={`sticky-header nav-header ${showScrolling ? "scrolling" : ""} ${showReveal ? "reveal-header" : ""}`}>
         <NavbarContainer>
           <LogoWrapper>
             <StyledLink to="/" className="site-logo">
               <motion.div whileHover={{ scale: 1.05 }}>
                 balthazar.
-                <span style={{ color: '#56B381' }}>dev</span>
+                <span style={{ color: "#56B381" }}>dev</span>
               </motion.div>
             </StyledLink>
           </LogoWrapper>
@@ -145,44 +141,28 @@ export default function Navbar() {
           </MenuBtnWrapper>
 
           <LinkWrapper>
-            <AnchorLink
-              className="links nav-link-text"
-              to="/#work"
-              title="Work"
-            >
+            <AnchorLink className="links nav-link-text" to="/#work" title="Work">
               <motion.div whileHover={{ scale: 1.1 }} className="nav-link-hov">
                 work.
               </motion.div>
             </AnchorLink>
-            <AnchorLink
-              className="links nav-link-text"
-              to="/#skills"
-              title="Work"
-            >
+            <AnchorLink className="links nav-link-text" to="/#skills" title="Work">
               <motion.div whileHover={{ scale: 1.1 }} className="nav-link-hov">
                 skills.
               </motion.div>
             </AnchorLink>
-            <AnchorLink
-              className="links nav-link-text"
-              to="/contact/"
-              title="Work"
-            >
+            <AnchorLink className="links nav-link-text" to="/contact/" title="Work">
               <motion.div whileHover={{ scale: 1.1 }} className="nav-link-hov">
                 contact.
               </motion.div>
             </AnchorLink>
-            <a
-              className="links nav-link-text"
-              href={downloadFile}
-              target="_blank"
-            >
+            <a className="links nav-link-text" href={downloadFile} target="_blank">
               <motion.div whileHover={{ scale: 1.1 }} className="nav-link-hov">
                 resume.
               </motion.div>
             </a>
 
-            <div style={{ marginLeft: '20px' }}>
+            <div style={{ marginLeft: "20px" }}>
               <ToggleSwitch />
             </div>
           </LinkWrapper>
